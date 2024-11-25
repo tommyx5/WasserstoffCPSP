3c4ab7a2e22a66b38c81771ede6f39c737a7a74b
Tommy Huynh <thuynh@gmx.de>
2024-11-25 23:09:46 +0100

    dwater finished

--- src/distillation_system_sum/run.py
+++ src/distillation_system_sum/run.py
@@ -5,51 +5,51 @@ from random import seed, randint
 from mqtt.mqtt_wrapper import MQTTWrapper
 
 # MQTT topic for publishing sensor data
-FILTER_SYSTEM_SUM_DATA = "data/fwater/sum"
+FILTER_SYSTEM_SUM_DATA = "data/dwater/sum"
 
 # MQTT topic for receiving tick messages
 COUNT_FILTER_SYSTEM = 10 #?
-FILTER_SYSTEM_DATA = "data/fwater/"
+FILTER_SYSTEM_DATA = "data/dwater/"
 FILTER_SYSTEM_DATA_LIST = []
 for i in range(COUNT_FILTER_SYSTEM):
     FILTER_SYSTEM_DATA_LIST.append(FILTER_SYSTEM_DATA+str(i))
 
-SUM_FWATER = 0
+SUM_DWATER = 0
 COUNT = 0
-MEAN_FWATER = 0
-FWATER_LIST = []
+MEAN_DWATER = 0
+DWATER_LIST = []
 COUNT_TICKS_MAX = 24*4
 COUNT_TICKS = 0
 for i in range(COUNT_TICKS_MAX):
-    FWATER_LIST.append(0)
+    DWATER_LIST.append(0)
 
 def calc_mean():
-    global SUM_FWATER, MEAN_FWATER, FWATER_LIST
+    global SUM_DWATER, MEAN_DWATER, DWATER_LIST
     summe = 0
     for i in range(COUNT_TICKS_MAX):
-        summe += FWATER_LIST[i]
-    MEAN_FWATER = round(summe / COUNT_TICKS_MAX, 2)
+        summe += DWATER_LIST[i]
+    MEAN_DWATER = round(summe / COUNT_TICKS_MAX, 2)
 
 
 def on_message_power(client, userdata, msg):
     global FILTER_SYSTEM_SUM_DATA
     global COUNT, COUNT_TICKS_MAX, COUNT_TICKS
-    global SUM_FWATER, MEAN_FWATER, FWATER_LIST
+    global SUM_DWATER, MEAN_DWATER, DWATER_LIST
     global COUNT_FILTER_SYSTEM
 
     payload = json.loads(msg.payload) 
-    fwater = payload["fwater"]
+    dwater = payload["dwater"]
     timestamp = payload["timestamp"]
     if COUNT % COUNT_FILTER_SYSTEM == 0:
-        SUM_FWATER = fwater
+        SUM_DWATER = dwater
     else:
-        SUM_FWATER += fwater
-        FWATER_LIST[COUNT_TICKS] = SUM_FWATER
+        SUM_DWATER += dwater
+        DWATER_LIST[COUNT_TICKS] = SUM_DWATER
         calc_mean()
         COUNT_TICKS = (COUNT_TICKS + 1) % COUNT_TICKS_MAX
         if COUNT == COUNT_FILTER_SYSTEM-1:
             # Extract the timestamp from the tick message and decode it from UTF-8
-            data = {"fwater": SUM_FWATER, "mean_fwater": MEAN_FWATER, "timestamp": timestamp}
+            data = {"dwater": SUM_DWATER, "mean_dwater": MEAN_DWATER, "timestamp": timestamp}
             # Publish the data to the chaos sensor topic in JSON format
             client.publish(FILTER_SYSTEM_SUM_DATA, json.dumps(data))
     COUNT = (COUNT + 1) % COUNT_FILTER_SYSTEM
