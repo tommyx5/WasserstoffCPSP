2c43ffdbb2fe0476f6a3138c74284304bcced5aa
DanyBoi22 <129411216+DanyBoi22@users.noreply.github.com>
2024-12-10 01:47:13 +0100

    new communication protocol. water pipe new

--- src/water_pipe/run.py
+++ src/water_pipe/run.py
@@ -14,11 +14,14 @@ def getenv_or_exit(env_name, default="default"):
 TICK = getenv_or_exit('TOPIC_TICK_GEN_TICK', 'default')
 TOPIC_WATER = getenv_or_exit('TOPIC_WATER_PIPE_SUPPLY', "default")
 TOPIC_REQUEST = getenv_or_exit('TOPIC_WATER_PIPE_WATER_REQUEST', "default")
+TOPIC_CONFIRM = "confirm"
+TOPIC_ERROR = "error"
 
 # Water volume (in m^3) that can be supplied by the pipe
 WATER = float(getenv_or_exit('WATER_PIPE_SUPPLY', 0.0))
 
 available_water = 0 # total volume of water that can be supplied
+blocked_water = 0 # amount of water that was requested but not yet confirmed
 
 def on_message_tick(client, userdata, msg):
     """
@@ -37,6 +40,7 @@ def on_message_tick(client, userdata, msg):
     available_water = WATER # update available water
 
     data = {
+        "msg": "available",
         "watersupply": available_water,  # Ensure this is a float, which is JSON serializable
         "timestamp": timestamp
     }
@@ -49,10 +53,6 @@ def calculate_supply(demand):
     water_supplied = 0
     if(demand < available_water):
         water_supplied = demand
-        available_water = available_water - demand
-    else:
-        water_supplied = available_water
-        available_water = 0
     return water_supplied
 
 def on_message_request(client, userdata, msg):
@@ -64,6 +64,7 @@ def on_message_request(client, userdata, msg):
     client (MQTT client): The MQTT client instance
     msg (MQTTMessage): The message containing the tick timestamp
     """
+    global blocked_water, available_water
     
     #extracting the timestamp and other data
     payload = json.loads(msg.payload)
@@ -73,12 +74,37 @@ def on_message_request(client, userdata, msg):
 
     water_suplied = calculate_supply(demand)
     
-    data = {
+    if(water_suplied == demand):
+        # publish accept msg
+        data = {
+        "msg": "accept",
         "watersupply": water_suplied, 
         "timestamp": timestamp
-    }
+        }
+
+        blocked_water =+ water_suplied # increase blocked water
+        available_water =- water_suplied # reduce available water
+    else:
+        # publish reject msg    
+        data = {
+        "msg": "reject", 
+        "timestamp": timestamp
+        }    
 
     client.publish(topic, json.dumps(data))
+
+def on_message_confirm(client, userdata, msg):
+    global blocked_water
+    payload = json.loads(msg.payload)
+    demand = payload["waterdemand"]
+    blocked_water =- demand
+
+def on_message_error(client, userdata, msg):
+    global blocked_water, available_water
+    payload = json.loads(msg.payload)
+    demand = payload["waterdemand"]
+    blocked_water =- demand
+    available_water =+ demand
 
 def main():
     """
@@ -91,8 +117,12 @@ def main():
     
     mqtt.subscribe(TICK)
     mqtt.subscribe(TOPIC_REQUEST)
+    mqtt.subscribe(TOPIC_CONFIRM)
+    mqtt.subscribe(TOPIC_ERROR)
     mqtt.subscribe_with_callback(TICK, on_message_tick)
     mqtt.subscribe_with_callback(TOPIC_REQUEST, on_message_request)
+    mqtt.subscribe_with_callback(TOPIC_CONFIRM, on_message_confirm)
+    mqtt.subscribe_with_callback(TOPIC_ERROR, on_message_error)
     
     try:
         # Start the MQTT loop to process incoming and outgoing messages
