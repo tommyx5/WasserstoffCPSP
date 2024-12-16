bc7223a6384a6d636d562bdfe88026276e08c5d0
DanyBoi22 <129411216+DanyBoi22@users.noreply.github.com>
2024-12-16 21:59:59 +0100

    filter plant rework for new documentation

--- src/filter_plant/run.py
+++ src/filter_plant/run.py
@@ -22,12 +22,22 @@ TOPIC_WATER_RECIEVE = getenv_or_exit("TOPIC_FILTER_PLANT_WATER_RECIEVE", "defaul
 TOPIC_POWER_REQUEST = getenv_or_exit("TOPIC_POWER_SUM_POWER_REQUEST", "default") # topic to request power
 TOPIC_POWER_RECIEVE = getenv_or_exit("TOPIC_FILTER_PLANT_POWER_RECIEVE", "default") + ID # must be followed by filter plant id
 TOPIC_FILTERED_WATER_SUPPLY = getenv_or_exit("TOPIC_FILTER_PLANT_FILTERED_WATER_SUPPLY", "default") + ID # must be followed by filter plant id
+TOPIC_KPI = getenv_or_exit("TOPIC_FILTER_PLANT_KPI", "default") + ID # Topic to post kpis
+TOPIC_PLANED_AMOUNT = getenv_or_exit("TOPIC_FILTER_PLANT_PLANED_AMOUNT", "default") + ID # topic to receive produce planed amount for the next tick
 
-TOPIC_FOR_POWER = getenv_or_exit("TOPIC_POWER_SUM_POWER_SUM_DATA", "default")
-TOPIC_FOR_DEP = getenv_or_exit('TOPIC_WATER_PIPE_SUPPLY', "default")
+PLANED_POWER_DEMAND = POWER_DEMAND
+PLANED_WATER_DEMAND = WATER_DEMAND
+PLANED_WATER_SUPPLY = WATER_SUPPLY
 
-AVAILABLE = 0 
+POWER_SUPPLIED = 0
+WATER_SUPPLIED = 0
 TIMESTAMP = 0
+
+STATUS = False
+EFFICIENCY = 0
+PRODUCTION = 0
+CURRENT_PERFORMANCE = 0
+
 
 class StateManager:
     STATES = ['WAITING_FOR_POWER', 'WAITING_FOR_DEPENDENCY', 'WAITING_FOR_TICK', 'READY_TO_PROCESS', 'PROCESSING', 'DONE']
@@ -89,45 +99,65 @@ class StateManager:
 
 state_manager = StateManager()
 
-def process(client):
-    global AVAILABLE, TOPIC_FILTERED_WATER_SUPPLY, TIMESTAMP
-    filtered_water_supply = filter_water(AVAILABLE)
-    AVAILABLE = 0
-                    
-    data = { 
-        "filteredwatersupply": filtered_water_supply, 
-        "timestamp": TIMESTAMP
+def send_request_msg(client, request_topic, timestamp, plant_id, reply_topic, amount):
+    data = {
+        "timestamp": timestamp, 
+        "plant_id": plant_id, 
+        "reply_topic": reply_topic, 
+        "amount": amount
     }
-    # Publish the data to the topic in JSON format
-    client.publish(TOPIC_FILTERED_WATER_SUPPLY, json.dumps(data))
+    client.publish(request_topic, json.dumps(data))
 
+def send_supply_msg(client, supply_topic, timestamp, amount):
+    data = {
+        "timestamp": timestamp,  
+        "amount": amount
+    }
+    client.publish(supply_topic, json.dumps(data))
 
-def filter_water(water_supplied):
-    global WATER_DEMAND, WATER_SUPPLY
+def send_kpi_msg(client, kpi_topic, timestamp, plant_id, status, eff, prod, cper):
+    data = {
+        "timestamp": timestamp, 
+        "plant_id": plant_id,
+        "status": status,
+        "eff": eff, 
+        "prod": prod, 
+        "cper": cper
+    }
+    client.publish(kpi_topic, json.dumps(data))
 
+def water_demand_on_supplied_power():
+    global POWER_SUPPLIED, PLANED_POWER_DEMAND, PLANED_WATER_DEMAND
+
+    if POWER_SUPPLIED >= PLANED_POWER_DEMAND:
+        water_demand = PLANED_WATER_DEMAND
+    else:
+        water_demand = (POWER_SUPPLIED / PLANED_POWER_DEMAND) * PLANED_WATER_DEMAND
+
+    return water_demand
+
+def produce_on_supplied_water():
+    global WATER_SUPPLIED, PLANED_WATER_DEMAND, PLANED_WATER_SUPPLY
     filtered_water = 0
 
-    if water_supplied < WATER_DEMAND:
-        filtered_water = water_supplied
+    if WATER_SUPPLIED < PLANED_WATER_DEMAND:
+        filtered_water = WATER_SUPPLIED
     else:
-        filtered_water = WATER_SUPPLY
+        filtered_water = PLANED_WATER_DEMAND
     return filtered_water
 
-def on_message_water_received(client, userdata, msg):
-    """
-    Callback function that processes messages from the water received topic.
-    It processes how much water is received from water pipe and generates the coresponding volume of filtered volume.
-    After that publishes it along with the timestamp.
-    """
-    global AVAILABLE, state_manager, TIMESTAMP
+def calculate_kpis():
+    return False
 
+def on_message_tick(client, userdata, msg):
+    global state_manager, TIMESTAMP, TOPIC_POWER_REQUEST, ID, TOPIC_POWER_RECIEVE, PLANED_POWER_DEMAND
+
+    # get timestamp from tick msg and request power   
     payload = json.loads(msg.payload)
-    timestamp = payload["timestamp"]
-    water_supply = payload["watersupply"]
-    
-    TIMESTAMP = timestamp
-    AVAILABLE = water_supply
-    state_manager.receive_dependency()  # Mark water as received in state manager
+    TIMESTAMP = payload["timestamp"]
+    send_request_msg(client, TOPIC_POWER_REQUEST, TIMESTAMP, ID, TOPIC_POWER_RECIEVE, PLANED_POWER_DEMAND)
+
+    #state_manager.receive_tick()
 
 def on_message_power_received(client, userdata, msg):
     """
@@ -138,26 +168,39 @@ def on_message_power_received(client, userdata, msg):
     client (MQTT client): The MQTT client instance
     msg (MQTTMessage): The message containing the tick timestamp
     """
-    global POWER_DEMAND
-    global state_manager, TIMESTAMP, TOPIC_FILTERED_WATER_SUPPLY
+    global state_manager, TIMESTAMP
+    global TOPIC_WATER_REQUEST, ID, TOPIC_WATER_RECIEVE, POWER_SUPPLIED
 
     payload = json.loads(msg.payload)
     timestamp = payload["timestamp"]
-    #power_supply = payload["powersupply"]
-    power_supply = payload["power"]
+    POWER_SUPPLIED = payload["amount"]
+    # Calculate water demand based on supplied power
+    water_demand = water_demand_on_supplied_power()
+
+    send_request_msg(client, TOPIC_WATER_REQUEST, TIMESTAMP, ID, TOPIC_WATER_RECIEVE, water_demand)
+
+    #state_manager.receive_power()
     
-    TIMESTAMP = timestamp
+
+def on_message_water_received(client, userdata, msg):
+    """
+    Callback function that processes messages from the water received topic.
+    It processes how much water is received from water pipe and generates the coresponding volume of filtered volume.
+    After that publishes it along with the timestamp.
+    """
+    global state_manager, TIMESTAMP, WATER_SUPPLIED, TOPIC_FILTERED_WATER_SUPPLY, TOPIC_KPI
+
+    payload = json.loads(msg.payload)
+    timestamp = payload["timestamp"]
+    WATER_SUPPLIED = payload["amount"]
+    # Calculate the amount of filtered water based on supplied water 
+    filtered_water_supply = produce_on_supplied_water()
     
-    if power_supply < POWER_DEMAND:
-        # publish zero topic
-        data = { 
-        "filteredwatersupply": 0, 
-        "timestamp": timestamp
-        }
-        client.publish(TOPIC_FILTERED_WATER_SUPPLY, json.dumps(data))
-    else:
-        state_manager.receive_power()
-    
+    send_supply_msg(client, TOPIC_FILTERED_WATER_SUPPLY, TIMESTAMP, filtered_water_supply)
+
+    calculate_kpis()
+    send_kpi_msg(client, TOPIC_KPI)
+    #state_manager.receive_dependency()  # Mark water as received in state manager
 
 def main():
     """
@@ -168,24 +211,18 @@ def main():
     # Initialize the MQTT client and connect to the broker
     mqtt = MQTTWrapper('mqttbroker', 1883, name='filter_plant_' + ID)
     
-
-    mqtt.subscribe(TOPIC_FOR_DEP)
-    mqtt.subscribe(TOPIC_FOR_POWER)
-    mqtt.subscribe_with_callback(TOPIC_FOR_DEP, on_message_water_received)
-    mqtt.subscribe_with_callback(TOPIC_FOR_POWER, on_message_power_received)
+    mqtt.subscribe(TICK)
+    mqtt.subscribe(TOPIC_POWER_RECIEVE)
+    mqtt.subscribe(TOPIC_WATER_RECIEVE)
+    mqtt.subscribe(TOPIC_PLANED_AMOUNT)
+    mqtt.subscribe_with_callback(TICK, on_message_tick)
+    mqtt.subscribe_with_callback(TOPIC_WATER_RECIEVE, on_message_water_received)
+    mqtt.subscribe_with_callback(TOPIC_POWER_RECIEVE, on_message_power_received)
+    #mqtt.subscribe_with_callback(TOPIC_PLANED_AMOUNT, on_message_plan)
     
     try:
-        while True:
-            if state_manager.is_ready_to_process():
-                if state_manager.start_processing():
-                    # Perform processing here
-                    #print("Processing water with sufficient energy and dependencies.")
-                    # 
-                    process(mqtt)
-                    
-                    state_manager.complete_processing()
-            
-            mqtt.loop(0.1) # loop every 100ms
+        # Start the MQTT loop to process incoming and outgoing messages
+        mqtt.loop_forever()
     except (KeyboardInterrupt, SystemExit):
         mqtt.stop()
         sys.exit("KeyboardInterrupt -- shutdown gracefully.")
